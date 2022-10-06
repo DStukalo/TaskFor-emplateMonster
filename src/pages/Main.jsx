d44a26d032ddd8dd56819fe5bf9a1b4eac75ec35
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-10-06 13:25:11 +0300

    feat: add state to main page, and render  this state to page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react';
+
 export const Main = () => {
+    const [photos, setPhotos] = useState(null);
+
+    useEffect(()=> {
+    const fetchData = async () => {
+        const response = await fetch('https://picsum.photos/v2/list?page=1&limit=10')
+        const data = await response.json()
+        console.log(data)
+        setPhotos(data)
+    }
+    fetchData()
+    },[])
+
     return (
         <div>
             <h1>Main page</h1>
             <p>Some text</p>
+            <ul>
+                {photos && photos.map(({id,author,url, download_url}) => (
+                    <li key={id}>
+                        <p>{author}</p>
+                        <img src={download_url} width='300' alt={author}/>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
