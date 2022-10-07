852e0de8d07f0463ef5ec719d0dbc5abc86a3bd7
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-10-07 13:23:09 +0300

    refactor: update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../src/img/logo.svg';
+
+import '../../src/styles/components/header.scss'
 
 export const Header = () => {
 	return (
-		<nav className="navigation">
- 			<Link to='/'>Main</Link>
-			<Link to='/favorite'>Favorite</Link>
-		</nav>
+		<header className='header'>
+			<h1 className='header__logo'>
+				<Link to='/'>
+						<Logo />
+				</Link>
+			</h1>
+			<nav className="header__navigation">
+				<NavLink className='header__navigation-item' end to='/'>Main</NavLink>
+				<NavLink className='header__navigation-item' to='/favorite'>Favorite</NavLink>
+			</nav>
+		</header>
 	);
 }
