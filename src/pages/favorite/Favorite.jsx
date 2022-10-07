2cec2035a43f0e2ef9cbb8e2ab66c7e71e0ef26c
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-10-07 15:45:35 +0300

    refactor: update favorite page

--- src/pages/favorite/Favorite.jsx
+++ src/pages/favorite/Favorite.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from 'react';
+
+import { ReactComponent as LogoHeart } from '../../img/heart.svg';
+import './Favorite.scss'
+
 export const Favorite = () => {
+    const [state, setState] = useState(null);
+    
+    useEffect(() => {
+        if(localStorage.getItem('favorite')) {
+            const arrayOfPhoto = localStorage.getItem('favorite').split(';');
+            arrayOfPhoto.pop();
+            setState(arrayOfPhoto);
+        }
+    }, []);
+console.log(state)
     return (
-        <div>
-            <h1>Favorite</h1>
-            <p>Some text</p>
+        <div className='favorite'>
+            <section className='favorite__list'>
+                {state && state.map((el,i) => (
+                    <div key={i} className='favorite__item'>
+                        <img src={el} height='300' className='favorite__list__item-img' alt="favorite-photo"/>
+                    </div>
+                ))}
+                {!state && 
+                <div>
+                    <h2>Favorite</h2>
+                    You don`t add anything to favorite
+                </div>}
+            </section>
         </div>
     );
 };
