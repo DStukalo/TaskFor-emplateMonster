e4e71e1283f10414bd2ee66cfa47ade97a6d418c
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-10-07 11:04:14 +0300

    refactor: add more page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Favorite } from './pages/favorite/Favorite';
 import { Main } from './pages/main/Main';
+import { More } from './pages/more/MorePage';
 import { NotFoundPage } from './pages/NotFoundPage';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
                 <Route path='/' element={<Layout />}>
                     <Route index element={<Main />} />
                     <Route path='favorite' element={<Favorite />} />
+                    <Route path='more' element={<More />} />
                     <Route path='*' element={<NotFoundPage />} />
                 </Route>
             </Routes>
