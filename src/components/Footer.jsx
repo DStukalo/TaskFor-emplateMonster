207bebcc37b42cbc2e7dad10b7d15e4219c39f3d
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-10-07 15:52:07 +0300

    feat: update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
+import '../../src/styles/components/footer.scss'
+
 export const Footer = () => {
 	return (
-    <footer>
-      2022 &#169;
+    <footer className='footer'>
+      <div className='footer__item'>
+        <a href="https://www.linkedin.com/in/dmytro-stukalo-799b50250/">Author →</a>
+      </div>
+      <div className='footer__item'>
+      2022 <sup>&#169;</sup>
+      </div>
     </footer>
 	);
 }
