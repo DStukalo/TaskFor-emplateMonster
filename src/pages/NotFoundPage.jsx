3a3bdd5c0c7f549bf8e41aa0489b1fe1ab387374
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-12-07 12:40:32 +0200

    refactor: update 404 page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export const NotFoundPage = () => {
+    const [second, setSecond] = useState(5);
+	const navigate = useNavigate();
+    function tic() {
+		if (second > 0) setSecond(second - 1);
+	}
+
+	function updateTime() {
+		const timeId = setInterval(tic, 1000);
+		function redirectToWelcome() {
+			clearInterval(timeId);
+			navigate('/');
+		}
+		setTimeout(redirectToWelcome, 5000);
+	}
+	updateTime();
     return (
+        <section>
         <div>
-            This page doesn't found. Go <a href='/'>Main page</a>
+        This page Not Found.
         </div>
+        <div>
+        You will be redirected to the home page via
+            {' '}
+            <span>{second}</span>
+        </div>
+    </section>
     );
 };
