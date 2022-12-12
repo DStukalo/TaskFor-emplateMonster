c07966463aed980deed4f56887057b01525502c3
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-12-12 20:43:47 +0200

    feat: add component Gallery

---
+++ src/components/Galery/Gallery.jsx
@@ -0,0 +1,20 @@
+import { useSelector } from 'react-redux'
+
+import { PhotoCard } from './PhotoCard/PhotoCard'
+import { Preloader } from '../Preloader/Preloader'
+import styles from './Gallery.module.scss'
+
+export const Gallery = () => {
+	const {photos, isLoading} = useSelector(state => state.photos);
+
+	return (
+		<section className={styles.gallery}>
+			{!isLoading ?
+				(photos && photos.map(({id,author, download_url}) => (
+					<PhotoCard key={id} id={id} author={author} download_url={download_url}/>
+				))) : null
+			}
+		{isLoading ? ( <Preloader />) : null}
+</section>
+	)
+}
