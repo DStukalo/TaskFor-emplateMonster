bbd0685d76d5ca6bc2b159c121693da742a5b6ca
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-12-12 19:36:55 +0200

    refactor: update structure of project, add modular scss

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
-import '../../src/styles/components/footer.scss'
+import styles from './Footer.module.scss';
 
 export const Footer = () => {
 	return (
-    <footer className='footer'>
-      <div className='footer__item'>
+    <footer className={styles.footer}>
+      <div className={styles.footer__item}>
         <a href="https://www.linkedin.com/in/dmytro-stukalo-799b50250/">Author →</a>
       </div>
-      <div className='footer__item'>
+      <div className={styles.footer__item}>
       2022 <sup>&#169;</sup>
       </div>
     </footer>
