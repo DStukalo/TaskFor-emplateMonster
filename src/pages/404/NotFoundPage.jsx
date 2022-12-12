0afac4e9c5b415b06fef666fc906e70219327b81
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-12-12 21:11:58 +0200

    refactor: update NotFoundPage, add styles for this

--- src/pages/404/NotFoundPage.jsx
+++ src/pages/404/NotFoundPage.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import styles from './NotFoundPage.module.scss';
+
 export const NotFoundPage = () => {
-    const [second, setSecond] = useState(5);
+	const [second, setSecond] = useState(5);
 	const navigate = useNavigate();
-    function tic() {
+	function tic() {
 		if (second > 0) setSecond(second - 1);
 	}
 
@@ -17,15 +19,16 @@ export const NotFoundPage = () => {
 		setTimeout(redirectToWelcome, 5000);
 	}
 	updateTime();
+
     return (
-        <section>
-        <div>
+        <section className={styles.section}>
+        <div className={styles.section_text}>
         This page Not Found.
         </div>
-        <div>
+        <div className={styles.section_text}> 
         You will be redirected to the home page via
             {' '}
-            <span>{second}</span>
+            <span className={styles.section_counter}>{second}</span>
         </div>
     </section>
     );
