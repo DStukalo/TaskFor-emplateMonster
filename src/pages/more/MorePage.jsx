bc14b89e55168174db3eb3b565af7808028c7033
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-12-12 21:07:09 +0200

    refactor: update MorePage. add modular scss

--- src/pages/more/MorePage.jsx
+++ src/pages/more/MorePage.jsx
@@ -1,37 +1,49 @@
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react';
 
-import './MorePage.scss'
+import { Preloader } from '../../components/Preloader/Preloader'
+import styles from './MorePage.module.scss';
 
 export const More = () => {
-  const location = useLocation();
-  const { from } = location.state;
-  const [photo, setPhoto] = useState(null); 
+	const location = useLocation();
+	const { from } = location.state;
+	const [photo, setPhoto] = useState(null); 
+	const navigate = useNavigate();
 
-  useEffect(()=> {
-    const fetchData = async () => {
-        const response = await fetch(`https://picsum.photos/id/${from.id}/info`);
-        const data = await response.json();
-        setPhoto(data);
-    }
-    fetchData();
-    }, [from])
-    
-  return (
-      <div className='more-wrapper'>
-        {photo 
-          ? <section className='more'>
-              <h4 className='more__title'>Author: {photo.author}</h4>
-              <div className='more__img'>
-                <img src={photo.download_url} alt={photo.author}/>
-              </div>
-              <p className='more__description'><a href={photo.url}>Photo on Unsplash →</a></p>
-              <p className='more__description'> Width real photo : {photo.width}</p>
-              <p className='more__description'> Height real photo : {photo.height}</p>
-            </section>
-          : <div>Some problems</div>
-        }
-         
-      </div>
-  );
+	useEffect(()=> {
+		const fetchData = async () => {
+				const response = await fetch(`https://picsum.photos/id/${from.id}/info`);
+				const data = await response.json();
+				setPhoto(data);
+		}
+		fetchData();
+	}, [from]);
+
+	const onClickBtn = () => {
+		navigate(-1);
+	}
+
+	return (
+		<section className={styles.wrapper}>
+			<header className={styles.header} >
+				<button className={styles.header__btn} onClick={onClickBtn}>Go back</button>
+				<h3 className={styles.header__title} >Photo from id {from.id}</h3>
+				<div></div>
+			</header>
+			{photo ? 
+				<section className={styles.more}>
+					<div className={styles.more__img}>
+						<img src={photo.download_url} alt=''/>
+					</div>
+					<div className={styles.more__description}>
+						<h4 className={styles.more__title}>Author: {photo.author}</h4>
+						<p className={styles.more__text}><a href={photo.url}>Photo on Unsplash →</a></p>
+						<p className={styles.more__text}> Width real photo : {photo.width}</p>
+						<p className={styles.more__text}> Height real photo : {photo.height}</p>
+					</div>
+					</section>
+				: <Preloader />
+			}
+		</section>
+	);
 };
