a41f964eacfc0c87af4da0322be4c2377b5af0fb
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-12-13 15:13:54 +0200

    refactor: update favorite page, refactor style, add container

--- src/pages/favorite/Favorite.jsx
+++ src/pages/favorite/Favorite.jsx
@@ -32,23 +32,25 @@ export const Favorite = () => {
 
 	return (
 		<div className={styles.favorite}>
-			<section className={styles.favorite__list}>
-				{state && state.length > 0 ?
-				 (state.map((el) => (
-					<div key={el} className={styles.favorite__item}>
-						<div className={styles.favorite__img} onClick={() => onClickImg(el)}>
-							<img src={el} alt="favorite"/>
+			<div className={styles.container}>
+				<section className={styles.favorite__list}>
+					{state && state.length > 0 ?
+					(state.map((el) => (
+						<div key={el} className={styles.favorite__item}>
+							<div className={styles.favorite__img} onClick={() => onClickImg(el)}>
+								<img src={el} alt="favorite"/>
+							</div>
+							<button className={styles.favorite__btn} onClick={(e) => onClickBtnDelete(e, el)}>Remove from favorite</button>
+						</div>)
+					)) :
+					(
+						<div className={styles.empty}>
+							<h2 className={styles.empty__title}>Favorite</h2>
+							<p className={styles.empty__description}>You don`t add anything to favorite</p>
 						</div>
-						<button className={styles.favorite__btn} onClick={(e) => onClickBtnDelete(e, el)}>Remove from favorite</button>
-					</div>)
-				 )) :
-				 (
-					<div className={styles.empty}>
-						<h2 className={styles.empty__title}>Favorite</h2>
-						<p className={styles.empty__description}>You don`t add anything to favorite</p>
-					</div>
-				 )}
-			</section>
+					)}
+				</section>
+			</div>
 		</div>
 	);
 };
